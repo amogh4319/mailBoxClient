adac67932d5332d6c3ed4a1049efbb01a202a94a
amogh4319 <amogh4319@gmail.com>
2023-09-03 14:03:44 +0530

    adding 5 test cases to composepage

---
+++ src/components/ComposePage.test.js
@@ -0,0 +1,32 @@
+import {render,screen} from '@testing-library/react';
+import ComposePage from './ComposePage';
+
+describe('compose page component',()=>{
+    test('rendering to text',()=>{
+        render(<ComposePage/>);
+        const totext=screen.getByText('To');
+        expect(totext).toBeInTheDocument();
+    })
+    test('rendering subject text',()=>{
+        render(<ComposePage/>);
+        const subjecttext=screen.getByText('Subject');
+        expect(subjecttext).toBeInTheDocument();
+    })
+    test('rendering to placeholder text',()=>{
+        render(<ComposePage/>);
+        const toplaceHoldertext=screen.getByPlaceholderText('example@gmail.com');
+        expect(toplaceHoldertext).toBeInTheDocument();
+    })
+    test('rendering number of buttons', async() => {
+        render(<ComposePage />);
+        const buttonList = await screen.findAllByRole('button');
+        expect(buttonList).toHaveLength(13);
+      })
+      test('submitting the form', () => {
+        render(<ComposePage />);
+        const sendButton = screen.getByRole('button', { name: 'Send' });
+        expect(sendButton).toBeInTheDocument();
+      })
+      
+   
+})
